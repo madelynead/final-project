19263808d7ef972e8d90cc4bae80c433c8e6504a
madelynead <mduran263@student.cccs.edu>
2025-05-13 00:33:38 -0500

    Add script and README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Final Project
+
+This is a bash script that prints numbers from 1 to 50.
---
+++ counting.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# A script to count from 1 to 50
+
+for i in {1..50}
+do
+  echo $i
+  sleep 1
+done
